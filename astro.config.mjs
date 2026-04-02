0a248ac7c9387bb1a15387ba7a8c86702fd11607
erwaren12 <230599747+erwaren12@users.noreply.github.com>
2026-04-02 21:16:21 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/serverless'; // Ini mesin Vercel-nya
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
   output: 'server',
-  adapter: vercel(), // Ini untuk menyalakan backend di awan
+  adapter: vercel(),
 });
