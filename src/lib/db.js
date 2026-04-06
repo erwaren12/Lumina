ad21b506966eee031c0837421b45f31ee1c477d4
erwaren12 <230599747+erwaren12@users.noreply.github.com>
2026-04-06 09:58:49 +0700

    sdvkj

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,12 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false // Baris ini wajib ada agar Aiven mengizinkan koneksi dari Vercel
+    rejectUnauthorized: false
   }
 });
-
-export default pool;
